9867b7a1718748d7fcaf92db4697595b1372714a
javimar <javimardeveloper@gmail.com>
2018-03-04 22:35:16 +0100

    Prevent card from being opened twice

--- js/app.js
+++ js/app.js
@@ -155,6 +155,12 @@
 
  function addCardToOpenList(li, cardName)
  {
+   // if card is already open, we cannot push it again into the list
+   if(li.classList.contains(openCard()))
+   {
+     return;
+   }
+
    // card is open
    li.classList.add(openCard());
 
